10c3627af5b54f643fc241b76a8df4a90f3e9b1c
Brandon <tricksterguy@live.com>
2016-02-10 09:16:20 -0800

    Fix #5?  Not a good idea to send sound data for channel1 wait then send sound data for subsequent channels

--- source/main.cpp
+++ source/main.cpp
@@ -17,18 +17,10 @@ extern "C"
 #include <sys/stat.h>
 #include <unistd.h>
 #include "config.hpp"
+#include "version.hpp"
 
 #define CONSOLE_WIDTH 50
-#define CONSOLE_HEIGHT 28
-
-std::vector<std::string> files;
-unsigned int current_index = 0;
-
-volatile bool runThreads = true;
-/// Handle signaling more data is ready to be played
-Handle bufferReadyConsumeRequest;
-/// Handle signaling more data is ready to be decoded
-Handle bufferReadyProduceRequest;
+#define CONSOLE_HEIGHT (28 - 1)
 
 struct stream_buffer
 {
@@ -36,18 +28,94 @@ struct stream_buffer
     unsigned int samples;
 };
 
-// At any point in time one of these will be playing
+struct stream_filename
+{
+    VGMSTREAM* stream;
+    std::string filename;
+};
+
+enum class PlayMode
+{
+    /// Grabs samples piece by piece and plays it
+    STREAM = 0,
+    /// Loads entire song into memory and then plays it
+    LOAD = 1,
+};
+
+std::vector<std::string> files;
+unsigned int current_index = 0;
+PlayMode mode = PlayMode::STREAM;
+
+volatile bool runThreads = true;
+/// Handle signaling more data is ready to be played
+Handle bufferReadyConsumeRequest;
+/// Handle signaling more data is ready to be decoded
+Handle bufferReadyProduceRequest;
+
+// At any point in time in stream mode one of these will be playing
 // and the other will be used for unraveling channel data from vgmstream
 stream_buffer playBuffer1;
 stream_buffer playBuffer2;
 // Raw samples from vgmstream
 sample* rawSampleBuffer = NULL;
 
-struct stream_filename
+
+Result csndPlaySoundMulti(int chn, u32 flags, u32 sampleRate, float vol, float pan, const std::vector<sample*>& play_data, u32 size)
 {
-    VGMSTREAM* stream;
-    std::string filename;
-};
+	if (!(csndChannels & BIT(chn)))
+		return 1;
+
+	std::vector<u32> play_paddr;
+	play_paddr.reserve(play_data.size());
+
+	int encoding = (flags >> 12) & 3;
+	int loopMode = (flags >> 10) & 3;
+
+
+	if (!loopMode) flags |= SOUND_ONE_SHOT;
+
+	if (encoding != CSND_ENCODING_PSG)
+	{
+		for (const auto& addr : play_data)
+            play_paddr.push_back(osConvertVirtToPhys(addr));
+
+		/*if (data0 && encoding == CSND_ENCODING_ADPCM)
+		{
+			int adpcmSample = ((s16*)data0)[-2];
+			int adpcmIndex = ((u8*)data0)[-2];
+			CSND_SetAdpcmState(chn, 0, adpcmSample, adpcmIndex);
+		}*/
+	}
+
+	u32 timer = CSND_TIMER(sampleRate);
+	if (timer < 0x0042) timer = 0x0042;
+	else if (timer > 0xFFFF) timer = 0xFFFF;
+	flags &= ~0xFFFF001F;
+	u32 volumes = CSND_VOL(vol, pan);
+
+	int channels = play_data.size() / (loopMode ? 2 : 1);
+
+	if (loopMode)
+    {
+        for (int i = 0; i < channels; i++)
+        {
+            u32 paddr0 = play_paddr[2 * i];
+            u32 paddr1 = play_paddr[2 * i + 1];
+            CSND_SetChnRegs(flags | SOUND_ENABLE | SOUND_CHANNEL(chn+i) | (timer << 16), paddr0, paddr1, size, volumes, volumes);
+            size -= paddr1 - paddr0;
+            CSND_SetBlock(chn, 1, paddr1, size);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < channels; i++)
+        {
+            CSND_SetChnRegs(flags | SOUND_ENABLE | SOUND_CHANNEL(chn+i) | (timer << 16), play_paddr[i], 0, size, volumes, volumes);
+        }
+    }
+
+	return csndExecCmds(true);
+}
 
 void getFiles(void)
 {
@@ -65,7 +133,7 @@ void getFiles(void)
     std::sort(files.begin(), files.end());
 }
 
-void playMusic(void* arg)
+void streamMusic(void* arg)
 {
     stream_filename* strm_file = static_cast<stream_filename*>(arg);
     VGMSTREAM* vgmstream = strm_file->stream;
@@ -81,10 +149,7 @@ void playMusic(void* arg)
     svcWaitSynchronization(bufferReadyConsumeRequest, U64_MAX);
     svcClearEvent(bufferReadyConsumeRequest);
     // Play it
-    for (int i = 0; i < channels; i++)
-    {
-        csndPlaySound(0x8 + i, SOUND_ONE_SHOT | SOUND_FORMAT_16BIT, vgmstream->sample_rate, 1.0, 0.0, (u32*)buffer->channels[i], NULL, buffer->samples * sizeof(sample));
-    }
+    csndPlaySoundMulti(0x8, SOUND_ONE_SHOT | SOUND_FORMAT_16BIT, vgmstream->sample_rate, 1.0, 0.0, buffer->channels, buffer->samples * sizeof(sample));
     svcSignalEvent(bufferReadyProduceRequest);
 
     while (runThreads)
@@ -94,9 +159,8 @@ void playMusic(void* arg)
             if (audio_playback_status == 0)
             {
                 // Make sure the buffer is flushed.
-                unsigned int samples = buffer->samples;
                 for (int i = 0; i < channels; i++)
-                    GSPGPU_FlushDataCache(buffer->channels[i], samples * sizeof(sample));
+                    GSPGPU_FlushDataCache(buffer->channels[i], buffer->samples * sizeof(sample));
 
                 // Wait for sound data here
                 svcWaitSynchronization(bufferReadyConsumeRequest, U64_MAX);
@@ -108,16 +172,15 @@ void playMusic(void* arg)
                 else
                     buffer = &playBuffer1;
 
-                samples = buffer->samples;
-                for (int i = 0; i < channels; i++)
-                {
-                    csndPlaySound(0x8 + i, SOUND_ONE_SHOT | SOUND_FORMAT_16BIT, vgmstream->sample_rate, 1.0, 0.0, (u32*)buffer->channels[i], NULL, samples *sizeof(sample));
-                }
+                csndPlaySoundMulti(0x8, SOUND_ONE_SHOT | SOUND_FORMAT_16BIT, vgmstream->sample_rate, 1.0, 0.0, buffer->channels, buffer->samples * sizeof(sample));
                 audio_playback_status = 1;
                 svcSignalEvent(bufferReadyProduceRequest);
             }
         }
     }
+
+    for (int i = 0; i < channels; i++)
+        GSPGPU_FlushDataCache(buffer->channels[i], buffer->samples * sizeof(sample));
 
     for (int i = 0; i < channels; i++)
     {
@@ -207,6 +270,7 @@ void refresh(void)
             (files.size() < CONSOLE_HEIGHT ? 0 : files.size() - CONSOLE_HEIGHT) :
             current_index;
     end = std::min(start + CONSOLE_HEIGHT, files.size() - 1);
+    printf("3ds-vgmstream v%s - play mode: %s\n", version_str, (mode == PlayMode::STREAM ? "Stream" : "Play"));
     for (unsigned int i = start; i <= end; i++)
     {
         printf(i == current_index ? ">" : " ");
@@ -244,6 +308,11 @@ std::string select_file(void)
             current_index = std::min(current_index + 1, files.size() - 1);
             refresh();
         }
+        if (kDown & KEY_R)
+        {
+            mode = (mode == PlayMode::STREAM) ? PlayMode::LOAD : PlayMode::STREAM;
+            refresh();
+        }
 
         gfxFlushBuffers();
         gfxSwapBuffers();
@@ -258,7 +327,7 @@ std::string select_file(void)
     return ret;
 }
 
-bool play_file(const std::string& filename)
+bool stream_file(const std::string& filename)
 {
     if (filename.empty())
     {
@@ -296,7 +365,7 @@ bool play_file(const std::string& filename)
     Thread produceThread;
     svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
     produceThread = threadCreate(decodeThread, &strm_file, 4 * 1024, prio-1, -2, false);
-    musicThread = threadCreate(playMusic, &strm_file, 4 * 1024, prio-1, -2, false);
+    musicThread = threadCreate(streamMusic, &strm_file, 4 * 1024, prio-1, -2, false);
 
     bool ret = false;
     while (aptMainLoop())
@@ -332,6 +401,81 @@ bool play_file(const std::string& filename)
     return ret;
 }
 
+bool play_file(const std::string& filename)
+{
+    if (filename.empty())
+    {
+        printf("No file selected\n");
+        return true;
+    }
+
+    VGMSTREAM* vgmstream = init_vgmstream(filename.c_str());
+    if (!vgmstream)
+    {
+        printf("Bad file %s\n", filename.c_str());
+        return true;
+    }
+
+    const int channels = vgmstream->channels;
+    const u32 stream_samples_amount = get_vgmstream_play_samples(1, 0, 0, vgmstream);
+    u32 buffer_size = stream_samples_amount * vgmstream->channels * sizeof(sample);
+
+    rawSampleBuffer = static_cast<sample*>(linearAlloc(buffer_size));
+    sample* buffer = static_cast<sample*>(linearAlloc(buffer_size));
+    for (int i = 0; i < channels; i++)
+        playBuffer1.channels.push_back(buffer + i * max_samples);
+
+    render_vgmstream(rawSampleBuffer, stream_samples_amount, vgmstream);
+
+    playBuffer1.samples = stream_samples_amount;
+    for (u32 i = 0; i < playBuffer1.samples; i++)
+    {
+        for (int j = 0; j < channels; j++)
+        {
+            playBuffer1.channels[j][i] = rawSampleBuffer[i * channels + j];
+        }
+    }
+
+    std::vector<sample*> addrs;
+    for (int i = 0; i < channels; i++)
+    {
+        sample* start = playBuffer1.channels[i];
+        sample* loop_start = start + vgmstream->loop_start_sample;
+        addrs.push_back(start);
+        addrs.push_back(loop_start);
+    }
+    csndPlaySoundMulti(0x8, SOUND_REPEAT | SOUND_FORMAT_16BIT, vgmstream->sample_rate, 1.0, 0.0, addrs, playBuffer1.samples * sizeof(sample));
+
+
+    bool ret = false;
+    while (aptMainLoop())
+    {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START || kDown & KEY_B)
+        {
+            ret = kDown & KEY_START;
+            break;
+        }
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+
+        gspWaitForVBlank();
+    }
+
+    linearFree(rawSampleBuffer);
+    linearFree(buffer);
+    playBuffer1.channels.clear();
+
+    for (int i = 0; i < channels; i++)
+    {
+        CSND_SetPlayState(0x8 + i, 0);
+        CSND_UpdateInfo(true);
+    }
+
+    return ret;
+}
+
 int main(void)
 {
     gfxInitDefault();
@@ -349,7 +493,7 @@ int main(void)
     while (!exit)
     {
         std::string filename = select_file();
-        exit = play_file(filename);
+        exit = mode == PlayMode::STREAM ? stream_file(filename) : play_file(filename);
     }
 
     csndExit();
---
+++ source/version.hpp
@@ -0,0 +1,7 @@
+#ifndef VERSION_HPP
+#define VERSION_HPP
+
+const char* version_str = "0.1.4";
+
+#endif
+
